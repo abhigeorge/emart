41b719ec93c9f83cbcf24e526b406de02d4571be
abhigeorge <abhigeorgekalladayil@gmail.com>
2021-05-05 15:33:19 +0530

    setup admin theme and menu items

---
+++ resources/views/backend/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<script src="{{ asset('backend/assets/bundles/libscripts.bundle.js')}}"></script>
+<script src="{{ asset('backend/assets/bundles/vendorscripts.bundle.js')}}"></script>
+
+<script src="{{ asset('backend/assets/bundles/jvectormap.bundle.js')}}"></script> <!-- JVectorMap Plugin Js -->
+<script src="{{ asset('backend/assets/bundles/morrisscripts.bundle.js')}}"></script><!-- Morris Plugin Js -->
+<script src="{{ asset('backend/assets/bundles/knob.bundle.js')}}"></script> <!-- Jquery Knob-->
+
+<script src="{{ asset('backend/assets/bundles/mainscripts.bundle.js')}}"></script>
+<script src="{{ asset('backend/assets/js/index4.js')}}"></script>
